75fff0beb41a1e137f7bc9234b045ecbc26033c7
alekseyti <68515628+alekseyti@users.noreply.github.com>
2024-09-19 22:02:26 +0300

    Add files via upload

---
+++ svg-icon.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-icon',
+  templateUrl: './svg-icon.component.html',
+  styleUrls: ['./svg-icon.component.scss']
+})
+export class SvgIconComponent implements OnInit {
+
+
+  @HostBinding('style.-webkit-mask-image')
+  private _path!: string;
+
+  @Input()
+  public set path(filePath: string) {
+    this._path = `url("${filePath}")`;
+  }
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
